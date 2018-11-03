6072a6405c81e7ccdb81f2e6bc3245135a4d8dcf
leepuppychow <leepuppychow@gmail.com>
2018-11-03 00:17:32 -0600

    Creates database connection package

---
+++ database/database.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"jay_medtronic/env"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func init() {
+	fmt.Println("Database connection initialized")
+	DB, _ = sql.Open("postgres", env.DevelopmentDB)
+	err := DB.Ping()
+	if err != nil {
+		log.Fatal("Cannot establish a connection with the database", err)
+	}
+}
