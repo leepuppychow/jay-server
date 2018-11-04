f9d9d0bc10abf5e8bff72a01ac074b3e0459b1ee
leepuppychow <leepuppychow@gmail.com>
2018-11-04 13:53:11 -0700

    CreateToken and ValidateToken working with tests passing

--- models/user.go
+++ models/user.go
@@ -3,10 +3,13 @@ package user
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"jay_medtronic/database"
+	"jay_medtronic/env"
 	"log"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/raja/argon2pw"
 )
 
@@ -19,6 +22,11 @@ type User struct {
 type UserResponse struct {
 	Message string `json:"message"`
 	Token   string `json:"token,omitempty"`
+}
+
+type UserClaims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
 }
 
 var password string
@@ -47,23 +55,47 @@ func MissingFields(user User) error {
 	return err
 }
 
+func CreateToken(email string) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{"email": email})
+	// SignedString requires byte array not string as argument type
+	tokenString, err := token.SignedString([]byte(env.JWTSecret))
+	if err != nil {
+		fmt.Println("Error creating token:", err)
+	}
+	return tokenString
+}
+
+func ValidateToken(tokenString string) (interface{}, bool) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(env.JWTSecret), nil
+	})
+	if err != nil {
+		fmt.Println("Error validating token:", err)
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); token.Valid && ok {
+		return claims["email"], true
+	}
+	return "Invalid token", false
+}
+
 func Create(body io.Reader) (UserResponse, error) {
 	var user User
 	err := json.NewDecoder(body).Decode(&user)
 	hashedPW := HashPassword(user.Password)
 	err = MissingFields(user)
-	
+
 	if err != nil {
-		return UserResponse{ Message: err.Error() }, err
-	} 
+		return UserResponse{Message: err.Error()}, err
+	}
 
 	_, err = database.DB.Exec("INSERT INTO users (email, password) VALUES ($1, $2)", user.Email, hashedPW)
+
 	if err != nil {
-		return UserResponse{ Message: err.Error() }, err
+		return UserResponse{Message: err.Error()}, err
 	} else {
 		return UserResponse{
 			Message: "User created successfully",
-			Token:   "WHEEE",
+			Token:   CreateToken(user.Email),
 		}, nil
 	}
 }
@@ -74,20 +106,23 @@ func Login(body io.Reader) (UserResponse, error) {
 	err = MissingFields(user)
 
 	if err != nil {
-		return UserResponse{ Message: err.Error() }, err
+		return UserResponse{Message: err.Error()}, err
 	}
 
 	err = database.DB.QueryRow("SELECT password FROM users WHERE email=$1", user.Email).Scan(&password)
+
 	if err != nil {
-		return UserResponse{ Message: err.Error() }, err
+		return UserResponse{Message: err.Error()}, err
 	}
+
 	hashedPassword := password
 	_, err = ValidatePassword(hashedPassword, user.Password)
+
 	if err != nil {
-		return UserResponse{ Message: err.Error() }, err
+		return UserResponse{Message: err.Error()}, err
 	}
 	return UserResponse{
 		Message: "User is authenticated",
-		Token:   "WHEEE",
+		Token:   CreateToken(user.Email),
 	}, nil
 }
