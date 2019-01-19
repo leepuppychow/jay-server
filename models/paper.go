8528a66ad03e4bb1cc9e9fa7454d12a1e9593d3d
leepuppychow <leepuppychow@gmail.com>
2019-01-19 13:04:42 -0700

    Adjusts Paper model to new schema (with submissions)

--- models/paper.go
+++ models/paper.go
@@ -12,28 +12,32 @@ import (
 )
 
 type Paper struct {
-	Id                      int               `json:"id"`
-	Title                   string            `json:"title"`
-	Study_Id                int               `json:"study_id"`
-	Journal_Id              int               `json:"journal_id"`
-	InitialRequestEvaluated string            `json:"initial_request_evaluated"`
-	ManuscriptDrafted       string            `json:"manuscript_drafted"`
-	ManuscriptSubmitted     string            `json:"manuscript_submitted"`
-	ManuscriptAccepted      string            `json:"manuscript_accepted"`
-	ManuscriptEpub          string            `json:"manuscript_epub"`
-	ManuscriptPrinted       string            `json:"manuscript_printed"`
-	SubmissionAttempts      int               `json:"submission_attempts"`
-	IntExtErp               string            `json:"int_ext_erp"`
-	CreatedAt               string            `json:"created_at"`
-	UpdatedAt               string            `json:"updated_at"`
-	Study                   string            `json:"study"`
-	Journal                 string            `json:"journal"`
-	Devices                 []Device          `json:"devices"`
-	Device_Ids              []int             `json:"device_ids"`
-	Authors                 []Author          `json:"authors"`
-	Author_Ids              []int             `json:"author_ids"`
-	Figures                 []Figure          `json:"figures"`
-	DataRequestForms        []DataRequestForm `json:"data_request_forms"`
+	Id                      int          `json:"id"`
+	Title                   string       `json:"title"`
+	Study_Id                int          `json:"study_id"`
+	Journal_Id              int          `json:"journal_id"`
+	InitialRequestEvaluated string       `json:"initial_request_evaluated"`
+	DrfRounds               int          `json:"drf_rounds"`
+	DrfCompleted            string       `json:"drf_completed"`
+	DrfRequestedDelivery    string       `json:"drf_requested_delivery"`
+	DrfActualDelivery       string       `json:"drf_actual_delivery"`
+	DataRefinementComplete  string       `json:"data_refinement_complete"`
+	ManuscriptDrafted       string       `json:"manuscript_drafted"`
+	ManuscriptAccepted      string       `json:"manuscript_accepted"`
+	ManuscriptEpub          string       `json:"manuscript_epub"`
+	ManuscriptPrinted       string       `json:"manuscript_printed"`
+	IntExtErp               string       `json:"int_ext_erp"`
+	CreatedAt               string       `json:"created_at"`
+	UpdatedAt               string       `json:"updated_at"`
+	Study                   string       `json:"study"`
+	Journal                 string       `json:"journal"`
+	Submissions             []Submission `json:"submissions"`
+	Submission_Ids          []int        `json:"submission_ids"`
+	Devices                 []Device     `json:"devices"`
+	Device_Ids              []int        `json:"device_ids"`
+	Authors                 []Author     `json:"authors"`
+	Author_Ids              []int        `json:"author_ids"`
+	Figures                 []Figure     `json:"figures"`
 }
 
 type PaperResponse struct {
@@ -52,12 +56,15 @@ func GetAllPapers(authToken string) ([]Paper, error) {
 		study_id                  int
 		journal_id                int
 		initial_request_evaluated pq.NullTime
+		drf_rounds                int
+		drf_completed             pq.NullTime
+		drf_requested_delivery    pq.NullTime
+		drf_actual_delivery       pq.NullTime
+		data_refinement_complete  pq.NullTime
 		manuscript_drafted        pq.NullTime
-		manuscript_submitted      pq.NullTime
 		manuscript_accepted       pq.NullTime
 		manuscript_epub           pq.NullTime
 		manuscript_printed        pq.NullTime
-		submission_attempts       int
 		int_ext_erp               string
 		created_at                time.Time
 		updated_at                time.Time
@@ -78,17 +85,20 @@ func GetAllPapers(authToken string) ([]Paper, error) {
 	for rows.Next() {
 		err = rows.Scan(
 			&id,
-			&title,
 			&study_id,
 			&journal_id,
+			&title,
+			&int_ext_erp,
 			&initial_request_evaluated,
+			&drf_rounds,
+			&drf_completed,
+			&drf_requested_delivery,
+			&drf_actual_delivery,
+			&data_refinement_complete,
 			&manuscript_drafted,
-			&manuscript_submitted,
 			&manuscript_accepted,
 			&manuscript_epub,
 			&manuscript_printed,
-			&submission_attempts,
-			&int_ext_erp,
 			&created_at,
 			&updated_at,
 			&study,
@@ -100,11 +110,9 @@ func GetAllPapers(authToken string) ([]Paper, error) {
 
 		authorsChannel := make(chan []Author)
 		figuresChannel := make(chan []Figure)
-		drfChannel := make(chan []DataRequestForm)
 		devicesChannel := make(chan []Device)
 		go GetAuthorsForPaper(id, authorsChannel)
 		go GetFiguresForPaper(id, figuresChannel)
-		go GetDataRequestFormsForPaper(id, drfChannel)
 		go GetDevicesForPaper(id, devicesChannel)
 
 		paper := Paper{
@@ -113,12 +121,15 @@ func GetAllPapers(authToken string) ([]Paper, error) {
 			Study_Id:                study_id,
 			Journal_Id:              journal_id,
 			InitialRequestEvaluated: NullTimeCheck(initial_request_evaluated),
+			DrfRounds:               drf_rounds,
+			DrfCompleted:            NullTimeCheck(drf_completed),
+			DrfRequestedDelivery:    NullTimeCheck(drf_requested_delivery),
+			DrfActualDelivery:       NullTimeCheck(drf_actual_delivery),
+			DataRefinementComplete:  NullTimeCheck(data_refinement_complete),
 			ManuscriptDrafted:       NullTimeCheck(manuscript_drafted),
-			ManuscriptSubmitted:     NullTimeCheck(manuscript_submitted),
 			ManuscriptAccepted:      NullTimeCheck(manuscript_accepted),
 			ManuscriptEpub:          NullTimeCheck(manuscript_epub),
 			ManuscriptPrinted:       NullTimeCheck(manuscript_printed),
-			SubmissionAttempts:      submission_attempts,
 			IntExtErp:               int_ext_erp,
 			CreatedAt:               created_at.String(),
 			UpdatedAt:               updated_at.String(),
@@ -126,7 +137,6 @@ func GetAllPapers(authToken string) ([]Paper, error) {
 			Journal:                 journal,
 			Authors:                 <-authorsChannel,
 			Figures:                 <-figuresChannel,
-			DataRequestForms:        <-drfChannel,
 			Devices:                 <-devicesChannel,
 		}
 		papers = append(papers, paper)
@@ -144,16 +154,19 @@ func FindPaper(paperId int, authToken string) (interface{}, error) {
 	// }
 	var (
 		id                        int
-		title                     string
 		study_id                  int
 		journal_id                int
+		title                     string
 		initial_request_evaluated pq.NullTime
+		drf_rounds                int
+		drf_completed             pq.NullTime
+		drf_requested_delivery    pq.NullTime
+		drf_actual_delivery       pq.NullTime
+		data_refinement_complete  pq.NullTime
 		manuscript_drafted        pq.NullTime
-		manuscript_submitted      pq.NullTime
 		manuscript_accepted       pq.NullTime
 		manuscript_epub           pq.NullTime
 		manuscript_printed        pq.NullTime
-		submission_attempts       int
 		int_ext_erp               string
 		created_at                time.Time
 		updated_at                time.Time
@@ -169,17 +182,20 @@ func FindPaper(paperId int, authToken string) (interface{}, error) {
 	`
 	err := database.DB.QueryRow(queryString, paperId).Scan(
 		&id,
-		&title,
 		&study_id,
 		&journal_id,
+		&title,
+		&int_ext_erp,
 		&initial_request_evaluated,
+		&drf_rounds,
+		&drf_completed,
+		&drf_requested_delivery,
+		&drf_actual_delivery,
+		&data_refinement_complete,
 		&manuscript_drafted,
-		&manuscript_submitted,
 		&manuscript_accepted,
 		&manuscript_epub,
 		&manuscript_printed,
-		&submission_attempts,
-		&int_ext_erp,
 		&created_at,
 		&updated_at,
 		&study,
@@ -188,11 +204,9 @@ func FindPaper(paperId int, authToken string) (interface{}, error) {
 
 	authorsChannel := make(chan []Author)
 	figuresChannel := make(chan []Figure)
-	drfChannel := make(chan []DataRequestForm)
 	devicesChannel := make(chan []Device)
 	go GetAuthorsForPaper(id, authorsChannel)
 	go GetFiguresForPaper(id, figuresChannel)
-	go GetDataRequestFormsForPaper(id, drfChannel)
 	go GetDevicesForPaper(id, devicesChannel)
 
 	paper := Paper{
@@ -201,12 +215,15 @@ func FindPaper(paperId int, authToken string) (interface{}, error) {
 		Study_Id:                study_id,
 		Journal_Id:              journal_id,
 		InitialRequestEvaluated: NullTimeCheck(initial_request_evaluated),
+		DrfRounds:               drf_rounds,
+		DrfCompleted:            NullTimeCheck(drf_completed),
+		DrfRequestedDelivery:    NullTimeCheck(drf_requested_delivery),
+		DrfActualDelivery:       NullTimeCheck(drf_actual_delivery),
+		DataRefinementComplete:  NullTimeCheck(data_refinement_complete),
 		ManuscriptDrafted:       NullTimeCheck(manuscript_drafted),
-		ManuscriptSubmitted:     NullTimeCheck(manuscript_submitted),
 		ManuscriptAccepted:      NullTimeCheck(manuscript_accepted),
 		ManuscriptEpub:          NullTimeCheck(manuscript_epub),
 		ManuscriptPrinted:       NullTimeCheck(manuscript_printed),
-		SubmissionAttempts:      submission_attempts,
 		IntExtErp:               int_ext_erp,
 		CreatedAt:               created_at.String(),
 		UpdatedAt:               updated_at.String(),
@@ -214,7 +231,6 @@ func FindPaper(paperId int, authToken string) (interface{}, error) {
 		Journal:                 journal,
 		Authors:                 <-authorsChannel,
 		Figures:                 <-figuresChannel,
-		DataRequestForms:        <-drfChannel,
 		Devices:                 <-devicesChannel,
 	}
 
@@ -232,17 +248,20 @@ func CreatePaperQuery(p Paper) (int, error) {
 			study_id,
 			journal_id, 
 			initial_request_evaluated,
+			drf_rounds,
+			drf_completed,
+			drf_requested_delivery,
+			drf_actual_delivery,
+			data_refinement_complete,
 			manuscript_drafted,
-			manuscript_submitted,
 			manuscript_accepted,
 			manuscript_epub,
 			manuscript_printed,
-			submission_attempts,
 			int_ext_erp,
 			created_at,
 			updated_at
 		)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
 		RETURNING id
 	`
 	lastInsertId := 0
@@ -251,12 +270,15 @@ func CreatePaperQuery(p Paper) (int, error) {
 		p.Study_Id,
 		p.Journal_Id,
 		p.InitialRequestEvaluated,
+		p.DrfRounds,
+		p.DrfCompleted,
+		p.DrfRequestedDelivery,
+		p.DrfActualDelivery,
+		p.DataRefinementComplete,
 		p.ManuscriptDrafted,
-		p.ManuscriptSubmitted,
 		p.ManuscriptAccepted,
 		p.ManuscriptEpub,
 		p.ManuscriptPrinted,
-		p.SubmissionAttempts,
 		p.IntExtErp,
 	).Scan(&lastInsertId)
 
@@ -322,10 +344,10 @@ func SpecialCreatePaper(body io.Reader, authToken string) (interface{}, error) {
 			return GeneralResponse{Message: err.Error()}, err
 		}
 	}
-	return GeneralResponse{ Message: "Paper, author_papers, and device_papers created successfully"}, nil
+	return GeneralResponse{Message: "Paper, author_papers, and device_papers created successfully"}, nil
 }
 
-func UpdatePaper(id int, body io.Reader, authToken string) (GeneralResponse, error) {
+func UpdatePaper(paperId int, body io.Reader, authToken string) (GeneralResponse, error) {
 	// if !ValidToken(authToken) {
 	// 	return GeneralResponse{Message: "Unauthorized"}, errors.New("Unauthorized")
 	// }
@@ -338,28 +360,34 @@ func UpdatePaper(id int, body io.Reader, authToken string) (GeneralResponse, err
 			study_id = $3,
 			journal_id = $4,
 			initial_request_evaluated = $5,
-			manuscript_drafted = $6,
-			manuscript_submitted = $7,
-			manuscript_accepted = $8,
-			manuscript_epub = $9,
-			manuscript_printed = $10,
-			submission_attempts = $11,
-			int_ext_erp = $12,
+			drf_rounds = $6,
+			drf_completed = $7,
+			drf_requested_delivery = $8,
+			drf_actual_delivery = $9,
+			data_refinement_complete = $10,
+			manuscript_drafted = $11,
+			manuscript_accepted = $12,
+			manuscript_epub = $13,
+			manuscript_printed = $14,
+			int_ext_erp = $15,
 			updated_at = CURRENT_TIMESTAMP
 		WHERE id = $1
 	`
 	_, err = database.DB.Exec(queryString,
-		id,
+		paperId,
 		p.Title,
 		p.Study_Id,
 		p.Journal_Id,
 		p.InitialRequestEvaluated,
+		p.DrfRounds,
+		p.DrfCompleted,
+		p.DrfRequestedDelivery,
+		p.DrfActualDelivery,
+		p.DataRefinementComplete,
 		p.ManuscriptDrafted,
-		p.ManuscriptSubmitted,
 		p.ManuscriptAccepted,
 		p.ManuscriptEpub,
 		p.ManuscriptPrinted,
-		p.SubmissionAttempts,
 		p.IntExtErp,
 	)
 
