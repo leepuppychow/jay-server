52eb1708b19c6ecb5ef2f51d6a4c64a62addd42e
leepuppychow <leepuppychow@gmail.com>
2019-01-21 10:06:10 -0700

    Modifies DB connection to allow for prod/dev/test

--- database/database.go
+++ database/database.go
@@ -6,14 +6,24 @@ import (
 	"log"
 
 	"github.com/leepuppychow/jay_medtronic/env"
-	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
-func init() {
+func InitDB(environment string) {
+	var connection string
+	switch environment {
+	case "production":
+		connection = env.ProductionDB
+	case "development":
+		connection = env.DevelopmentDB
+	case "test":
+		connection = env.TestDB
+	default:
+		connection = env.DevelopmentDB
+	}
 	fmt.Println("Database connection initialized")
-	DB, _ = sql.Open("postgres", env.DevelopmentDB)
+	DB, _ = sql.Open("postgres", connection)
 	err := DB.Ping()
 	if err != nil {
 		log.Fatal("Cannot establish a connection with the database", err)
